e48237e114d9dbf6beadcb92fcc2bee67c048b34
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-10-30 19:00:11 -0300

    feat: add techs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,6 +12,14 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [respositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+  
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
   }
@@ -19,7 +28,44 @@ export default function App() {
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
+        <FlatList 
+          style={styles.repositoryContainer}
+          data={respositories}
+          keyExtractor={repo => repo.id}
+          renderItem={({item}) => (
+          <View>
+            <Text style={styles.repository}>{item.title}</Text>
+
+            <View style={styles.techsContainer}>
+              {item.techs.map(tech => (
+              <Text style={styles.tech}>
+                {tech}
+              </Text>
+              ))}
+            </View>
+
+            <View style={styles.likesContainer}>
+              <Text
+                style={styles.likeText}
+                // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                testID={`repository-likes-1`}
+              >
+                3 curtidas
+              </Text>
+            </View>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleLikeRepository(1)}
+              // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+              testID={`like-button-1`}
+            >
+              <Text style={styles.buttonText}>Curtir</Text>
+            </TouchableOpacity>
+          </View>
+          )}
+        />
+        {/* <View style={styles.repositoryContainer}>
           <Text style={styles.repository}>Repository 1</Text>
 
           <View style={styles.techsContainer}>
@@ -49,7 +95,7 @@ export default function App() {
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
-        </View>
+        </View> */}
       </SafeAreaView>
     </>
   );
